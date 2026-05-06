17cbff01fe2dd3bf5f1b54834714253801ba2fcb
Juan Jaramillo <info@juanjaramillo.tech>
2026-05-06 21:28:57 +0000

    Update deploy script

--- scripts/deploy_update.sh
+++ scripts/deploy_update.sh
@@ -23,6 +23,13 @@ sudo rm -rf .next
 echo "[4/6] Building the application..."
 sudo npm run build
 
+# Copy static files for standalone server
+# Copy static files for standalone server
+sudo cp -r public .next/standalone/
+sudo cp -r .next/static .next/standalone/.next/
+
+sudo mkdir -p .next/standalone/app && sudo cp app/critical.css .next/standalone/app/
+
 echo "[5/6] Restarting PM2 process 'topfinanzas-us-next'..."
 sudo pm2 restart topfinanzas-us-next
 
